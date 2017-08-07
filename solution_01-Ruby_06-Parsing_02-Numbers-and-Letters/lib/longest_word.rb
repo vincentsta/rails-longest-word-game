9cdca1b474ee56935d83a564e4a6b1b25fdb3297
Vincent Stammbach <vincentsta.dev@gmail.com>
2017-08-07 14:59:50 +0200

    exo ok ss optionnel

---
+++ solution_01-Ruby_06-Parsing_02-Numbers-and-Letters/lib/longest_word.rb
@@ -0,0 +1,43 @@
+require 'open-uri'
+require 'json'
+
+def generate_grid(grid_size)
+  Array.new(grid_size) { ('A'..'Z').to_a.sample }
+end
+
+def included?(guess, grid)
+  guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+end
+
+def compute_score(attempt, time_taken)
+  time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+end
+
+def run_game(attempt, grid, start_time, end_time)
+  result = { time: end_time - start_time }
+
+  score_and_message = score_and_message(attempt, grid, result[:time])
+  result[:score] = score_and_message.first
+  result[:message] = score_and_message.last
+
+  result
+end
+
+def score_and_message(attempt, grid, time)
+  if included?(attempt.upcase, grid)
+    if english_word?(attempt)
+      score = compute_score(attempt, time)
+      [score, "well done"]
+    else
+      [0, "not an english word"]
+    end
+  else
+    [0, "not in the grid"]
+  end
+end
+
+def english_word?(word)
+  response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+  json = JSON.parse(response.read)
+  return json['found']
+end
